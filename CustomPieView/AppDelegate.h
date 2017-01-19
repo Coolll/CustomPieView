1a2751827466a9c1d2577ee57711d82b79061ea1
龙培 <longpei@longpeidemacbook-pro.local>
2017-01-19 17:22:41 +0800

    Initial Commit

---
+++ CustomPieView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomPieView
+//
+//  Created by 龙培 on 17/1/19.
+//  Copyright © 2017年 龙培. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomPieView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomPieView
+//
+//  Created by 龙培 on 17/1/19.
+//  Copyright © 2017年 龙培. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
